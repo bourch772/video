5aecaeefc51c0287f4bf02de94ceeafcb742211c
bourch772 <bourch772@gmail.com>
2017-08-03 13:06:26 +0100

    Update and rename .gitignore to .video.php

---
+++ .gitignore/.video.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Scrolling controls for HTML5 video</title>
+  
+
+  
+</head>
+
+<body>
+  
+<div class="mobile_container">
+                <video preload="none" x-webkit-airplay="allow" playsinline="true" width="100%" height="100%" controls="" poster="<?php echo urldecode($_GET['poster']); ?>" src="<?php echo urldecode($_GET['src']); ?>"> </video>
+                
+            </div>
+
+</body>
+</html>
